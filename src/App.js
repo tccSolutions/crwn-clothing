586034dcd653ea62bba7e3f8e24432973719af47
tccSolutions <tim@tccs.tech>
2021-08-10 16:49:55 -0400

    added header and styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import {Homepage} from './pages/homepage/homepage.component'
-import './App.css';
-import { Hats } from './pages/hats/hats.component';
-import { Shop } from './pages/shop/shop.component';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { Homepage } from "./pages/homepage/homepage.component";
+import "./App.css";
+import { Hats } from "./pages/hats/hats.component";
+import { Shop } from "./pages/shop/shop.component";
+import { Header } from "./components/header/header.component";
 
 function App() {
   return (
-   <Router>
-       <Route exact path='/'><Homepage/></Route>
-       <Route path='/shop'><Shop/></Route>
-       <Route path='/shop/hats'><Hats/></Route>     
-   </Router>
-   
-   
+    <div>
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={Homepage}/>         
+        <Route path="/shop" component={Shop}/>          
+        <Route path="/shop/hats" component={Hats}/>         
+      </Switch>
+    </div>
   );
 }
 
